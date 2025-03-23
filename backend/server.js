56f1a2a9b801390c452eacc9b597eaed8cef7e22
HonoraTT <xiet92788@gmail.com>
2025-03-23 15:09:33 +0800

    后端搭建

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+const app = express();
+const PORT = 5000; // 你可以修改为任何端口
+
+// 使用 CORS 中间件
+app.use(cors());
+
+// 使用 body-parser 解析 JSON 格式的请求体
+app.use(bodyParser.json());
+
+// 设置一个简单的 GET 路由
+app.get("/", (req, res) => {
+    res.send("Hello, Express server is running!");
 });
 
-const PORT = 3000;
+// 启动服务器
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.get('/api/data', (req, res) => {
-    res.json({ message: '这里是你的 API 数据' });
-});
-  
