db804a8cc2ff2b46dcaaa98846aacea3409f7d2b
覃燚婕 <qyj@wust.edu.cn>
2025-03-30 17:06:18 +0800

    匠人页面

---
+++ frontend/src/artisan-detail/王树文.jsx
@@ -0,0 +1,14 @@
+// src/artisan-detail/辜国强.jsx
+import React from 'react';
+
+const 王树文= () => {
+  return (
+    <div>
+      <h1>王树文</h1>
+      <p>王树文，花丝镶嵌工艺大师，致力于传承传统的匠人技艺，历经多年研究与创新，作品广受欢迎。</p>
+      <p>详细故事：王树文的工艺历程充满了艰辛与成功，他的作品曾多次获得国内外奖项...</p>
+    </div>
+  );
+};
+
+export default 王树文;
---
+++ frontend/src/artisan-detail/董瑞京.jsx
@@ -0,0 +1,14 @@
+// src/artisan-detail/辜国强.jsx
+import React from 'react';
+
+const 董瑞京= () => {
+  return (
+    <div>
+      <h1>董瑞京</h1>
+      <p>董瑞京，花丝镶嵌工艺大师，致力于传承传统的匠人技艺，历经多年研究与创新，作品广受欢迎。</p>
+      <p>详细故事：董瑞京的工艺历程充满了艰辛与成功，他的作品曾多次获得国内外奖项...</p>
+    </div>
+  );
+};
+
+export default 董瑞京;
