0906bc2c24521adc91c872b1a0bf8d89fc2cd981
jolaaa999 <2843422418@qq.com>
2025-05-03 12:24:36 +0800

    后端地址

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://118.178.238.109:8000', // 后端API地址
+  baseURL: 'https://filigree.wanheit.com', // 后端API地址
   withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
